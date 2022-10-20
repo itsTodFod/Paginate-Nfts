a2bf88b55c0e988715bf3c468bd8d7a07e79256f
itsTodFod <96081283+itsTodFod@users.noreply.github.com>
2022-10-20 16:26:49 +0530

    Replace with my code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { Metaplex } from '@metaplex-foundation/js';
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
-import { useEffect, useState } from 'react';
 
 const connection = new Connection(clusterApiUrl('mainnet-beta'));
 const mx = Metaplex.make(connection);
 
-export default function Home() {
+const Home = () => {
   const [address, setAddress] = useState(
     'Geh5Ss5knQGym81toYGXDbH3MFU2JCMK7E4QyeBHor1b',
   );
@@ -19,13 +19,14 @@ export default function Home() {
   const perPage = 1;
 
   const fetchNFTs = async () => {
-    try {
+    // add some code here
+    try{
       setLoading(true);
       setCurrentView(null);
       const list = await mx.nfts().findAllByOwner(new PublicKey(address));
       setNftList(list);
       setCurrentPage(1);
-    } catch (e) {
+    } catch(e) {
       console.error(e);
     }
   };
@@ -34,21 +35,23 @@ export default function Home() {
     if (!nftList) {
       return;
     }
-
+    
     const execute = async () => {
-      const startIndex = (currentPage - 1) * perPage;
-      const endIndex = currentPage * perPage;
+      const startIndex = (currentPage-1)*perPage;
+      const endIndex = (currentPage)*perPage;
       await loadData(startIndex, endIndex);
       setCurrentView(nftList.slice(startIndex, endIndex));
       setLoading(false);
     };
     execute();
+    // add some code here
   }, [nftList, currentPage]);
 
   const loadData = async (startIndex, endIndex) => {
+    // add some code here
     const nftsToLoad = nftList.filter((nft, index) => {
       return (
-        index >= startIndex && index < endIndex && nft.metadataTask.isPending()
+        index>=startIndex && index<endIndex && nft.metadataTask.isPending() 
       );
     });
 
@@ -57,12 +60,12 @@ export default function Home() {
   };
 
   const changeCurrentPage = (operation) => {
+    // add some code here
     setLoading(true);
-    if (operation === 'next') {
-      setCurrentPage((prevValue) => prevValue + 1);
-    } else {
-      setCurrentPage((prevValue) => (prevValue > 1 ? prevValue - 1 : 1));
-    }
+    if(operation==='next')
+      setCurrentPage((prevValue)=>(prevValue+1));
+    else
+      setCurrentPage((prevValue)=>(prevValue-1));
   };
 
   return (
@@ -110,7 +113,9 @@ export default function Home() {
                 Prev Page
               </button>
               <button
-                disabled={nftList && Math.ceil(nftList.length / perPage) === currentPage}
+                disabled={
+                  nftList && Math.ceil(nftList.length / perPage) === currentPage
+                }
                 className={styles.styledButton}
                 onClick={() => changeCurrentPage('next')}
               >
@@ -122,4 +127,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Home;
